093e67d113558252e3d908d99ca2b771e9f027c8
Tirth Chudasama <dintro193@gmail.com>
2026-06-22 00:09:01 +0530

    ads issue solved

--- client/public/sw.js
+++ client/public/sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 11168622
+    "domain": "3nbf4.com",
+    "zoneId": 11168755
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
